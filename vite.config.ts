25cef786ec22147aa140b6858e4ebc53af6d20d9
Thiago <tmnascim@gmail.com>
2025-04-30 17:36:32 -0300

    Ajuste na build

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,13 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 }));
